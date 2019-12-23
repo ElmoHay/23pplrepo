4de8ff5599189685367b39383e90bc9788cf9629
ElmoHay <jmura@outlook.com>
2019-12-23 15:49:22 -0300

    wired to netflix eureka thru <dependencyManagement>

--- src/main/java/com/example/_23people/demo23ppl/DemoApplication.java
+++ src/main/java/com/example/_23people/demo23ppl/DemoApplication.java
@@ -1,8 +1,12 @@
 package com.example._23people.demo23ppl;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableAutoConfiguration
+@EnableDiscoveryClient
 @SpringBootApplication
 public class DemoApplication {
 
