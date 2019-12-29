bd23339b91c43113509f463d4aa8494d21b7f636
Jose Mura from Mac23ppl <jose.mura@23people.io>
2019-12-28 21:56:40 -0300

    unnecesary interfaces removal in repositories

--- src/main/java/com/example/_23people/demo23ppl/repositories/CourseRepository.java
+++ src/main/java/com/example/_23people/demo23ppl/repositories/CourseRepository.java
@@ -1,11 +1,10 @@
 package com.example._23people.demo23ppl.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.example._23people.demo23ppl.models.*;
 
-public interface CourseRepository extends PagingAndSortingRepository<Course, Long>, JpaRepository<Course, Long>
+public interface CourseRepository extends PagingAndSortingRepository<Course, Long>
 {
     //PagingAndSortingRepository also extends from CrudRepository
     //List<Course> findByLast_name(String last_name);
--- src/main/java/com/example/_23people/demo23ppl/repositories/StudentRepository.java
+++ src/main/java/com/example/_23people/demo23ppl/repositories/StudentRepository.java
@@ -1,14 +1,13 @@
 package com.example._23people.demo23ppl.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 import com.example._23people.demo23ppl.models.*;
 
-public interface StudentRepository extends PagingAndSortingRepository<Student, Long>, JpaRepository<Student, Long>
+public interface StudentRepository extends JpaRepository<Student, Long>
 {
     //PagingAndSortingRepository also extends from CrudRepository
     List<Student> findByCourseId(Long CourseId);
